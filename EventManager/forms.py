bd917f8f53f658def86d1b7908f1f5087b02dee9
Xt4lll <isip_s.a.ushakov@mpt.ru>
2024-11-15 19:43:59 +0300

    reg + auth

--- EventManager/forms.py
+++ EventManager/forms.py
@@ -7,17 +7,23 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ('username', 'email')
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.fields['password1'].help_text = ''
-            self.fields['password2'].help_text = ''
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = ''
+        self.fields['email'].help_text = ''
+        self.fields['password1'].help_text = ''
+        self.fields['password2'].help_text = ''
+        self.fields['username'].label = 'Логин'
+        self.fields['email'].label = 'Почта'
+        self.fields['password1'].label = 'Пароль'
+        self.fields['password2'].label = 'Подтверждение пароля'
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(
         widget=forms.TextInput(attrs={'placeholder': 'Логин'}),
-        label=''
+        label='Логин'
     )
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={'placeholder': 'Пароль'}),
-        label=''
+        label='Пароль'
     )
