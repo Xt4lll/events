6c18c7ee97c0e9822ac1c4adef20a3f33a6dc384
Xt4lll <isip_s.a.ushakov@mpt.ru>
2024-12-20 21:54:30 +0300

    new

--- EventManager/admin.py
+++ EventManager/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import *
+from .models import User, Area, Sponsor, Address, EventType, Event, Tickets, Cart, Order, Artist, Payment
+
 
 # Register your models here.
 
@@ -7,45 +8,56 @@ from .models import *
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'password', 'role')
 
+
 @admin.register(Area)
 class AreaAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
+
 
 @admin.register(Sponsor)
 class SponsorAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'description')
 
+
 @admin.register(Address)
 class AddressAdmin(admin.ModelAdmin):
     list_display = ('id', 'place_name', 'address')
+
 
 @admin.register(EventType)
 class EventTypeAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'description')
 
+
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'sponsor', 'place', 'type', 'event_date')
+
 
 @admin.register(Tickets)
 class EventAreaAdmin(admin.ModelAdmin):
     list_display = ('id', 'event', 'area', 'places', 'price')
 
+
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'ticket')
+
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'status')
 
+
 @admin.register(Artist)
 class ArtistAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'description')
 
+
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'first_name', 'last_name', 'card_number', 'event_info', 'purchase_date')
+
 
 # @admin.register(ActingArtist)
 # class ActingArtistAdmin(admin.ModelAdmin):
